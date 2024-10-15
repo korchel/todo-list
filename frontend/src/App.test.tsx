9ac80f9485de82730e19fc08c9005ccb5c778a0c
korchel <korchel21@gmail.com>
2024-10-15 16:40:11 +0300

    url changed

--- frontend/src/App.test.tsx
+++ frontend/src/App.test.tsx
@@ -1,33 +1,35 @@
 import React from 'react';
 import { render, screen, waitFor } from '@testing-library/react';
 import App from './App';
-import {server} from './mock/server';
+import { server } from './mock/server';
 import userEvent from '@testing-library/user-event';
 
 describe('App', () => {
-  
-  beforeAll(() => {
-    server.listen();
-  });
-  afterAll(() => {
-    server.close();
-  });
+	beforeAll(() => {
+		server.listen();
+	});
+	afterAll(() => {
+		server.close();
+	});
 
-  it('fetches, adds, removes, toggles todos', async () => {
-    render(<App />);
-    expect(await screen.findByText('Task1')).toBeInTheDocument();
+	it('fetches, adds, removes, toggles todos', async () => {
+		render(<App />);
+		expect(await screen.findByText('Task1')).toBeInTheDocument();
 
-    const input = await screen.findByRole('textbox');
-    userEvent.type(input, "task3{enter}");
-    expect(await screen.findByText('Task3')).toBeInTheDocument();
+		const input = await screen.findByRole('textbox');
+		userEvent.type(input, 'task3{enter}');
+		expect(await screen.findByText('Task3')).toBeInTheDocument();
 
-    const checkbox = await screen.findByLabelText('Task2');
-    userEvent.click(checkbox);
-    await waitFor(() => expect(checkbox).toBeChecked());
+		const checkbox = await screen.findByLabelText('Task2');
+		userEvent.click(checkbox);
+		await waitFor(() => {
+			expect(checkbox).toBeChecked();
+		});
 
-    const button = await screen.findByTestId('btn1');
-    userEvent.click(button);
-    await waitFor(() => expect(screen.queryByText('Task1')).not.toBeInTheDocument());
-  });
+		const button = await screen.findByTestId('btn1');
+		userEvent.click(button);
+		await waitFor(() => {
+			expect(screen.queryByText('Task1')).not.toBeInTheDocument();
+		});
+	});
 });
-
--- frontend/src/store/todosApi.ts
+++ frontend/src/store/todosApi.ts
@@ -4,63 +4,63 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 import { type ITask } from '../types';
 
 interface ItaskUpdateRequest {
-  id: number,
-  update: {
-    task?: string
-    done?: boolean
-  }
+	id: number;
+	update: {
+		task?: string;
+		done?: boolean;
+	};
 }
 
 export const todosApi = createApi({
-  reducerPath: 'todos',
-  baseQuery: fetchBaseQuery({ baseUrl: 'https://sleepypancake-todo-app-b1d577334394.herokuapp.com/api/v1/todos' }),
-  tagTypes: ['Task'],
-  endpoints: (builder) => ({
-    getTasks: builder.query<ITask[], void>({
-      query: () => ({
-        url: '',
-      }),
-      providesTags: ['Task'],
-    }),
+	reducerPath: 'todos',
+	baseQuery: fetchBaseQuery({ baseUrl: 'https://todo-api-5ves.onrender.com/api/v1/todos' }),
+	tagTypes: ['Task'],
+	endpoints: (builder) => ({
+		getTasks: builder.query<ITask[], void>({
+			query: () => ({
+				url: ''
+			}),
+			providesTags: ['Task']
+		}),
 
-    getTask: builder.query<ITask, number>({
-      query: (id) => ({
-        url: `:${id}`,
-      }),
-    }),
+		getTask: builder.query<ITask, number>({
+			query: (id) => ({
+				url: `:${id}`
+			})
+		}),
 
-    addTask: builder.mutation({
-      query: (newTask) => ({
-        url: '',
-        method: 'POST',
-        body: newTask,
-      }),
-      invalidatesTags: ['Task'],
-    }),
+		addTask: builder.mutation({
+			query: (newTask) => ({
+				url: '',
+				method: 'POST',
+				body: newTask
+			}),
+			invalidatesTags: ['Task']
+		}),
 
-    removeTask: builder.mutation({
-      query: (id) => ({
-        url: `${id}`,
-        method: 'DELETE',
-        body: id,
-      }),
-      invalidatesTags: ['Task'],
-    }),
+		removeTask: builder.mutation({
+			query: (id) => ({
+				url: `${id}`,
+				method: 'DELETE',
+				body: id
+			}),
+			invalidatesTags: ['Task']
+		}),
 
-    updateTask: builder.mutation<void, ItaskUpdateRequest>({
-      query: ({ id, update }) => ({
-        url: `${id}`,
-        method: 'PUT',
-        body: update,
-      }),
-      invalidatesTags: ['Task'],
-    })
-  }),
+		updateTask: builder.mutation<void, ItaskUpdateRequest>({
+			query: ({ id, update }) => ({
+				url: `${id}`,
+				method: 'PUT',
+				body: update
+			}),
+			invalidatesTags: ['Task']
+		})
+	})
 });
 
 export const {
-  useGetTasksQuery,
-  useAddTaskMutation,
-  useRemoveTaskMutation,
-  useUpdateTaskMutation,
+	useGetTasksQuery,
+	useAddTaskMutation,
+	useRemoveTaskMutation,
+	useUpdateTaskMutation
 } = todosApi;
