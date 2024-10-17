88dc58391ad67965983c6e32363e9f392f8c586c
Elena Korchuganova <korchel21@gmail.com>
2024-10-17 22:25:56 +0300

    docker added

--- frontend/src/store/todosApi.ts
+++ frontend/src/store/todosApi.ts
@@ -13,7 +13,7 @@ interface ItaskUpdateRequest {
 
 export const todosApi = createApi({
 	reducerPath: 'todos',
-	baseQuery: fetchBaseQuery({ baseUrl: 'https://todo-api-5ves.onrender.com/api/v1/todos' }),
+	baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost/todos' }),
 	tagTypes: ['Task'],
 	endpoints: (builder) => ({
 		getTasks: builder.query<ITask[], void>({
