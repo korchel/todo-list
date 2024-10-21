329b0db93e93f467799286580734553471b13688
korchel <korchel21@gmail.com>
2024-10-21 09:46:54 +0300

    prevent default added

--- frontend/src/components/Modal/EditModal/EditModal.tsx
+++ frontend/src/components/Modal/EditModal/EditModal.tsx
@@ -1,4 +1,4 @@
-import React, { type ChangeEventHandler, useState } from 'react';
+import React, { type ChangeEventHandler, type FormEventHandler, useState } from 'react';
 
 import { closeModal, getId, getText } from '../../../store/modalSlice';
 import { useDispatch, useSelector } from 'react-redux';
@@ -6,35 +6,46 @@ import { Form, Textarea, ButtonGroup, Button } from './styles';
 import { useUpdateTaskMutation } from '../../../store/todosApi';
 
 const EditModal: React.FC = () => {
-  const dispatch = useDispatch();
-  const text = useSelector(getText);
-  const id = useSelector(getId);
+	const dispatch = useDispatch();
+	const text = useSelector(getText);
+	const id = useSelector(getId);
 
-  const [newText, setNewText] = useState<string>(text);
-  const [updateTask] = useUpdateTaskMutation();
+	const [newText, setNewText] = useState<string>(text);
+	const [updateTask] = useUpdateTaskMutation();
 
-  const handleClose = (): void => {
-    dispatch(closeModal());
-  };
+	const handleClose = (): void => {
+		dispatch(closeModal());
+	};
 
-  const handleChange: ChangeEventHandler<HTMLTextAreaElement> = (e) => {
-    setNewText(e.target.value);
-  };
-  const handleSubmit = (): void => {
-    id && updateTask({ id, update: { task: newText } });
-  };
+	const handleChange: ChangeEventHandler<HTMLTextAreaElement> = (e) => {
+		setNewText(e.target.value);
+	};
+	const handleSubmit: FormEventHandler<HTMLFormElement> = (e): void => {
+		e.preventDefault();
+		id && updateTask({ id, update: { task: newText } });
+	};
 
-  return (
-    <>
-      <Form onSubmit={handleSubmit}>
-        <Textarea aria-label="Edit task" value={newText} onChange={(e) => { handleChange(e); }} />
-        <ButtonGroup>
-          <Button $green type="submit">Save</Button>
-          <Button $red onClick={handleClose}>Cancel</Button>
-        </ButtonGroup>
-      </Form>
-    </>
-  );
+	return (
+		<>
+			<Form onSubmit={handleSubmit}>
+				<Textarea
+					aria-label='Edit task'
+					value={newText}
+					onChange={(e) => {
+						handleChange(e);
+					}}
+				/>
+				<ButtonGroup>
+					<Button $green type='submit'>
+						Save
+					</Button>
+					<Button $red onClick={handleClose}>
+						Cancel
+					</Button>
+				</ButtonGroup>
+			</Form>
+		</>
+	);
 };
 
 export default EditModal;
